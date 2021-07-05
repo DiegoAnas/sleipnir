3732564a547d19696e8e7521b1dd1914bbe8aeb0
Diego <diegoanasrh@gmail.com>
2021-07-05 20:50:16 +0200

    DeviceOnly deployment, undeploys previous tasks

--- src/main/java/at/ac/tuwien/ec/scheduling/offloading/algorithms/group16/DeviceOnly.java
+++ src/main/java/at/ac/tuwien/ec/scheduling/offloading/algorithms/group16/DeviceOnly.java
@@ -4,19 +4,15 @@ package at.ac.tuwien.ec.scheduling.offloading.algorithms.group16;
 import at.ac.tuwien.ec.model.infrastructure.MobileCloudInfrastructure;
 import at.ac.tuwien.ec.model.infrastructure.computationalnodes.ComputationalNode;
 import at.ac.tuwien.ec.model.infrastructure.computationalnodes.MobileDevice;
-import at.ac.tuwien.ec.model.software.ComponentLink;
 import at.ac.tuwien.ec.model.software.MobileApplication;
 import at.ac.tuwien.ec.model.software.MobileSoftwareComponent;
 import at.ac.tuwien.ec.scheduling.offloading.OffloadScheduler;
 import at.ac.tuwien.ec.scheduling.offloading.OffloadScheduling;
-import at.ac.tuwien.ec.scheduling.offloading.algorithms.heftbased.utils.NodeRankComparator;
-import at.ac.tuwien.ec.scheduling.utils.RuntimeComparator;
 import at.ac.tuwien.ec.sleipnir.OffloadingSetup;
-import org.jgrapht.graph.DirectedAcyclicGraph;
 import scala.Tuple2;
 
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.HashSet;
 
 /**
  * Dummy Scheduler that runs all nodes in the mobile device
@@ -45,7 +41,7 @@ public class DeviceOnly extends OffloadScheduler {
     /**
      * Processor selection phase:
      * select the mobile device
-     * @return
+     * @return a (list of) deployment of task components
      */
 	@Override
 	public ArrayList<? extends OffloadScheduling> findScheduling() {
@@ -53,25 +49,28 @@ public class DeviceOnly extends OffloadScheduler {
 		/*scheduledNodes contains the nodes that have been scheduled for execution.
 		 * Once nodes are scheduled, they are taken from the PriorityQueue according to their runtime
 		 */
-		ArrayList<MobileSoftwareComponent> tasks = new ArrayList<>();
-		tasks.addAll(currentApp.getTaskDependencies().vertexSet());
-		ArrayList<OffloadScheduling> deployments = new ArrayList<OffloadScheduling>();
-
+		ArrayList<MobileSoftwareComponent> tasks = new ArrayList<>(currentApp.getTaskDependencies().vertexSet());
+		ArrayList<OffloadScheduling> deployments = new ArrayList<>();
 		//We initialize a new OffloadScheduling object, modelling the scheduling computer with this algorithm
-		OffloadScheduling scheduling = new OffloadScheduling(); 
+		OffloadScheduling scheduling = new OffloadScheduling();
 		//We check until there are nodes available for scheduling
-        boolean warned = false;
+        HashSet<String> outOfBatteryDevices = new HashSet<>();
 		for (MobileSoftwareComponent currTask : tasks){
-            MobileDevice userDevice = (MobileDevice) currentInfrastructure.getNodeById(currTask.getUserId());
+            ComputationalNode userDevice = (ComputationalNode) currentInfrastructure.getNodeById(currTask.getUserId());
             // deploy it in the mobile device (if enough resources are available)
             // Since capabilities and connectivity shouldn't be an issue, check only if energy budget allows it.
             double consumption = userDevice.getCPUEnergyModel().computeCPUEnergy(currTask, userDevice, currentInfrastructure);
-            if (consumption < userDevice.getEnergyBudget()) {
-                deploy(scheduling,currTask,userDevice);
+            if (consumption < ((MobileDevice)userDevice).getEnergyBudget()) {
+                    if (userDevice.isCompatible(currTask)){
+                        deploy(scheduling, currTask, userDevice);
+                    } else {
+                        userDevice.undeploy(userDevice.getAllocatedTasks().get(0)); // get the first task deployed and undeploy it
+                        deploy(scheduling, currTask, userDevice);
+                    }
             }else{
-                if (!warned) { // show the energy message only once
+                if (!outOfBatteryDevices.contains(userDevice.getId())) { // show the energy message only once (per phone)
+                    outOfBatteryDevices.add(userDevice.getId());
                     System.out.println("Mobile energy budget does not allow execution on device");
-                    warned = true;
                 }
             }
 			/*
